541bfe4d680699bc99e9a1072c2814fa424c49db
jiaxinyang2003 <yangj850@gmail.com>
2025-04-21 23:56:32 +0800

    v1

--- web.py
+++ web.py
@@ -1,23 +1,45 @@
-from flask import Flask, render_template
-from datetime import datetime
-
+from flask import Flask, render_template,request
+from datetime import datetime,timezone, timedelta
 app = Flask(__name__)
+
 
 @app.route("/")
 def index():
-    return "Hello 楊佳欣"
+    homepage = "<h1>楊佳欣Python網頁(Firestore)</h1>"
+    homepage += "<a href=/mis>MIS</a><br>"
+    homepage += "<a href=/today>顯示日期時間</a><br>"
+    homepage += "<a href=/welcome?nick=weng>傳送使用者暱稱</a><br>"
+    homepage += "<a href=/account>網頁表單傳值</a><br>"
+    homepage += "<a href=/about>楊佳欣簡介網頁</a><br>"
+    return homepage
 @app.route("/mis")
 def course():
     return "<h1>資訊管理導論</h1>"
 @app.route("/today")
 def today():
-    now = datetime.now()
+    tz = timezone(timedelta(hours=+8))
+    now = datetime.now(tz)
     return render_template("today.html", datetime = str(now))
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    return render_template("about8.html")
 
+@app.route("/welcome",methods=["GET"]) 
+def welcome():
+    user = request.values.get("nick")
+    w = request.values.get("work")
+    return render_template("welcome.html",name=user,work=w)
+
+@app.route("/account", methods=["GET", "POST"])
+def account():
+    if request.method == "POST":
+        user = request.form["user"]
+        pwd = request.form["pwd"]
+        result = "您輸入的帳號是：" + user + "; 密碼為：" + pwd 
+        return result
+    else:
+        return render_template("account.html")    
     
 
 if __name__ == "__main__":
